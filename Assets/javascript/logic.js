b0dfd0437aa58066371075dd9ba09161caec0641
Jesse Marino <jessemarino2020@gmail.com>
2022-04-07 15:34:45 -0700

    added readme, attempted to add logic for past,present,future timeblocks

--- Assets/javascript/logic.js
+++ Assets/javascript/logic.js
@@ -1,17 +1,21 @@
-var sBtn = document.getElementById("saveBtn");
 // display date at the top of the screen in the jumbotron
-var currentTime = moment().format("MMM Do, dddd, YYYY");
+var currentTime = moment().format("MMM Do, dddd, YYYY hh:mm");
 $("#currentDay").text(currentTime);
-// change color of timeblocks by whether it is past (grey), present(red), future(green)
+var currentHour = moment().format("H");
 
-// save data to local storage when save button is pressed
+for (var i = 0; i < $(".time-block").length; i++) {
+    if (currentHour < $(".hour".value)) {
+        $(".time-block").addClass("past");
+    }
+    else if (currentHour > $(".hour".value)) {
+        $(".time-block").addClass("future");
+    }
+    else (currentHour = (".hour".value)) ;{
+        $(".time-block").addClass("present");
+    }
+}
+
 $("saveBtn").click(function() {
        localStorage.setItem($("description")).JSON.stringify($("description"));
-    }
-);
+});
 
-
-
-
-
-    //var email = document.querySelector("#email").value
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # key_to_a_great_day
+
+My motivation for this project was to create an awesome calander. My motivation for this was to make something to help me keep track of my homework assignments. I learned how to use momment.js and all of its cool abilities. Please see attached picture of my webpage. 
+
+ ![picture of website](./Assets/images/calander.png))
+
+ URL:
