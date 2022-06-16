8dd941aececbacc0b7dfb2f54b4bfe84b657e892
Jesse Marino <jessemarino2020@gmail.com>
2022-06-16 11:06:38 -0700

    added to logic.js implementing color codes current hour, future hour, or past hour

--- Assets/javascript/logic.js
+++ Assets/javascript/logic.js
@@ -1,21 +1,60 @@
-// display date at the top of the screen in the jumbotron
-var currentTime = moment().format("MMM Do, dddd, YYYY hh:mm");
-$("#currentDay").text(currentTime);
-var currentHour = moment().format("H");
+$(function () {
+  // listen for save button clicks
+  $(".saveBtn").on("click", function () {
+    // get nearby values
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-for (var i = 0; i < $(".time-block").length; i++) {
-    if (currentHour < $(".hour".value)) {
-        $(".time-block").addClass("past");
-    }
-    else if (currentHour > $(".hour".value)) {
-        $(".time-block").addClass("future");
-    }
-    else (currentHour = (".hour".value)) ;{
-        $(".time-block").addClass("present");
-    }
-}
+    // save in localStorage
+    localStorage.setItem(time, value);
 
-$("saveBtn").click(function() {
-       localStorage.setItem($("description")).JSON.stringify($("description"));
+    // Show notification that item was saved to localStorage by adding class 'show'
+    $(".notification").addClass("show");
+
+    // Timeout to remove 'show' class after 5 seconds
+    setTimeout(function () {
+      $(".notification").removeClass("show");
+    }, 5000);
+  });
+
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  hourUpdater();
+
+  // set up interval to check if current time needs to be updated
+  setInterval(hourUpdater, 15000);
+
+  // load any saved data from localStorage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
-
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # key_to_a_great_day
 
-My motivation for this project was to create an awesome calander. My motivation for this was to make something to help me keep track of my homework assignments. I learned how to use momment.js and all of its cool abilities. Please see attached picture of my webpage. 
+My motivation for this project was to create an awesome calander. My motivation for this was to make something to help me keep track of my homework assignments. I learned how to use momment.js and all of its cool abilities. Please see attached picture of my webpage.
 
- ![picture of website](./Assets/images/calander.png))
+![picture of website](./assets/images/calander.png))
 
- URL: https://jessemarino.github.io/key_to_a_great_day/
+URL: https://jessemarino.github.io/key_to_a_great_day/
